2960aed190dd802de8cdd97c3989ff63dfeabd6a
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-28 16:51:09 -0700

    made settings component

---
+++ imports/ui/Settings/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {useSound, useSettings} from '../../hooks';
+
+const Settings = props => {
+
+    return (
+        <div className='settings-modal'>
+            <fieldset className='settings-audio'>
+                <label>
+                    Sound Effects
+                    <input type='checkbox' checked={props.sound} onChange={()=>props.setSound(prevSetting => !prevSetting)}></input>
+                    <span className='slider round'></span>
+                </label>
+                <label>
+                    Music
+                    <input type='checkbox' checked={props.music} onChange={()=>props.setMusic(prevSetting => !prevSetting)}></input>
+                    <span className='slider round'></span>
+                </label>
+            </fieldset>
+            <button className='btn-close' onClick={()=>props.setSettings(false)}>Close</button>
+        </div>
+    )
+}
+
+export default Settings;
