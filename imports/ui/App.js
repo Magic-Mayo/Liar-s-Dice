60f67cb4a3e57350880113f036c8fdb6001227bb
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-28 16:51:08 -0700

    added hooks for settings. added splash div. added settings component. added image for settings button

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,33 +4,56 @@ import Instruction from './Instruction';
 import Game from './Game';
 import Header from './Header';
 import Dice from './Dice';
+import Settings from './Settings';
+import {useSettings, useSound, useMusic} from '../hooks';
 
-const App = () => (
+const App = () => {
+    const [settings, setSettings] = useSettings();
+    const [sound, setSound] = useSound();
+    const [music, setMusic] = useMusic();
     
-    <Router>
-        <Header />
-        <Route exact path='/'>
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
-            <Link to='/instruction' className='instructions'>
-                <button>How to Play</button>
-            </Link>
-            <Link to='/start' className='start'>
-                <button>Start Game!</button>
-            </Link>
-        </Route>
-        <Switch>
-            <Route exact path='/instruction'>
-                <Instruction />
+    return (
+        <Router>
+            <Header />
+            <Route exact path='/'>
+                <div className='splash'>
+                    <Dice />
+                    <Dice />
+                    <Dice />
+                    <Dice />
+                    <Dice />
+                </div>
+                <Link to='/instruction' className='instructions'>
+                    <button>How to Play</button>
+                </Link>
+                <Link to='/game' className='start'>
+                    <button>Start Game!</button>
+                </Link>
             </Route>
-            <Route exact path='/start'>
-                <Game />
-            </Route>
-        </Switch>
-    </Router>
-);
+            {settings && 
+                <Settings
+                setSettings={setSettings}
+                setSound={setSound}
+                setMusic={setMusic}
+                sound={sound}
+                music={music}
+                />
+            }
+            <Switch>
+                <Route exact path='/instruction'>
+                    <Instruction />
+                </Route>
+                <Route path='/game'>
+                    <Game />
+                </Route>
+            </Switch>
+            <img
+            src='/wheel.PNG'
+            alt='settings'
+            className='settings-icon'
+            onClick={()=>setSettings(true)}></img>
+        </Router>
+    );
+}
 
 export default App;
