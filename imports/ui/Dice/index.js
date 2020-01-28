f9fbcfeae8edac419622b9f19914f232e53ecafd
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-28 16:51:09 -0700

    added style prop to style div and dice according to settings when game is started

--- imports/ui/Dice/index.js
+++ imports/ui/Dice/index.js
@@ -4,15 +4,19 @@ const setRandNum = () => Math.floor(Math.random() * 6) + 1;
 
 const Dice = props => {
     const randNum = setRandNum();
+    const dieSize = [100, 75, 50];
+    const pipSize = [15, 10, 5];
 
     return (
-        <div className={`die die-${randNum}`}>
-            {randNum > 0 && <span className='pip'></span>}
-            {randNum > 1 && <span className='pip'></span>}
-            {randNum > 2 && <span className='pip'></span>}
-            {randNum > 3 && <span className='pip'></span>}
-            {randNum > 4 && <span className='pip'></span>}
-            {randNum > 5 && <span className='pip'></span>}
+        <div
+        className={`die die-${randNum}`}
+        style={{width: `${dieSize[props.dice]}px`, height: `${dieSize[props.dice]}px`}}>
+            {randNum > 0 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
+            {randNum > 1 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
+            {randNum > 2 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
+            {randNum > 3 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
+            {randNum > 4 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
+            {randNum > 5 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
         </div>
     )
 }
