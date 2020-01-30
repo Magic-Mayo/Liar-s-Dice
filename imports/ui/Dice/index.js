37df0ae7c54e5ee27222ad9aeb784aa398ad952b
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-30 16:59:10 -0700

    gave box shadow and inverted colors of dice that are clicked on when choosing what to bet

--- imports/ui/Dice/index.js
+++ imports/ui/Dice/index.js
@@ -1,22 +1,54 @@
-import React from 'react';
-
-const setRandNum = () => Math.floor(Math.random() * 6) + 1;
+import React, { useState } from 'react';
 
 const Dice = props => {
-    const randNum = setRandNum();
-    const dieSize = [100, 75];
-    const pipSize = [19, 15];
-
+    const dieSize = [10, 8, 6];
+    const pipSize = [2, 1.7, 1.3];
+    
     return (
         <div
-        className={`die die-${randNum}`}
-        style={{width: `${dieSize[props.dice]}px`, height: `${dieSize[props.dice]}px`}}>
-            {randNum > 0 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
-            {randNum > 1 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
-            {randNum > 2 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
-            {randNum > 3 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
-            {randNum > 4 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
-            {randNum > 5 && <span className='pip' style={{width: `${pipSize[props.pip]}px`, height: `${pipSize[props.pip]}px`}}></span>}
+        className={`die die-${props.number} ${props.selection ? 'die-hover' : ''}`}
+        style={props.selection === props.number ? 
+            {boxShadow: '#222 -12px 18px 10px', width: `${dieSize[props.dice]}vh`, height: `${dieSize[props.dice]}vh`, backgroundColor: 'black'} :
+            {width: `${dieSize[props.dice]}vh`, height: `${dieSize[props.dice]}vh`}}
+        onClick={props.selectDie ? ()=>props.selectDie(props.number) : null}>
+            
+            {props.number > 0 && 
+            <span
+            className='pip'
+            style={props.selection === props.number ?
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`, backgroundColor: 'beige'} :
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`}}></span>}
+
+            {props.number > 1 && 
+            <span className='pip'
+            style={props.selection === props.number ?
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`, backgroundColor: 'beige'} :
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`}}></span>}
+
+            {props.number > 2 && 
+            <span className='pip'
+            style={props.selection === props.number ?
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`, backgroundColor: 'beige'} :
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`}}></span>}
+
+            {props.number > 3 && 
+            <span className='pip'
+            style={props.selection === props.number ?
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`, backgroundColor: 'beige'} :
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`}}></span>}
+
+            {props.number > 4 && 
+            <span className='pip'
+            style={props.selection === props.number ?
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`, backgroundColor: 'beige'} :
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`}}></span>}
+
+            {props.number > 5 && 
+            <span className='pip'
+            style={props.selection === props.number ?
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`, backgroundColor: 'beige'} :
+                {width: `${pipSize[props.pip]}vh`, height: `${pipSize[props.pip]}vh`}}></span>}
+
         </div>
     )
 }
