ac9724f966ffc9778e4f10501b5d56c695213d3a
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-31 01:33:24 -0700

    added an array of names to choose CPU name from

--- imports/ui/CPU/index.js
+++ imports/ui/CPU/index.js
@@ -2,17 +2,49 @@ import {useEffect, useState} from 'react';
 
 const setRandNum = () => Math.floor(Math.random() * 6) + 1;
 
-export const CPU = numDice => {
+const pickName = () => {
+    const names = [
+        "Wylie 'The Blue Fox' Yelland",
+        "Errol 'Jolly Roger' Seamen",
+        "Jake 'Salty Dog' Ninnies",
+        "Gordon 'Shiny Buckles' Horn",
+        "Nelson 'Gold Fiend' Docherty",
+        "Samuel 'Tall Story' Scott",
+        "Jacob 'Sly Dog' Craggs",
+        "Tobias 'Saucy Devil' Dogg",
+        "Samson 'Bonny Baby' Pigg",
+        "Wade 'Loud Limey' Locke",
+        'Esme "One-Eyed" Goodwin',
+        'Gail "Silver Tooth" Yelland',
+        'Bella "Ugly Mug" Vague',
+        'Pearl "Black Beard" Shakes',
+        'Peggy "Red Locks" Ripper',
+        `Stella "Rum Drunk" Rugged`,
+        'Harriet "Black Beard" Raw',
+        'Pearl "Rum Drunk" Raw',
+        'Lil "Coconut Crazy" Locke',
+        'Carrie "Dead Man" Brown'
+    ]
+
+    return names[Math.floor(Math.random()*19)+1];
+}
+
+export const CPU = (numDice, rolling) => {
     const [dice, setDice] = useState();
+    const [name, setName] = useState();
 
     useEffect(()=>{
         setDice([]);
         for(let i=0; i<numDice; i++){
         setDice(prevState=>[...prevState, setRandNum()])
         }
-    },[numDice])
+    },[rolling])
 
-    return [dice];
+    useEffect(()=>{
+        setName(pickName());
+    },[name])
+
+    return [dice, name];
 }
 
 export default CPU;
