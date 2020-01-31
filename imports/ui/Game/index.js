772a30c37c8cdd16d365f2826028a2b37b95c745
Magic-Mayo <mike.mayoii@gmail.com>
2020-01-31 01:33:24 -0700

    moved game parameters and user choices into their own component

--- imports/ui/Game/index.js
+++ imports/ui/Game/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import Dice from '../Dice';
-import {CPU} from '../CPU';
+import CPU from '../CPU';
+import Parameters from '../Parameters';
+import Choice from './Choice';
 
 const setRandNum = () => Math.floor(Math.random() * 6) + 1;
+const odds = () => Math.floor(Math.random()*100)+1;
 
 const Game = props => {
     const [players, setPlayers] = useState(2);
@@ -12,35 +15,32 @@ const Game = props => {
     const [rolling, setRolling] = useState(false);
     const [numChoice, setNumChoice] = useState(1);
     const [numArr, setNumArr] = useState([]);
-    const [lastNum, setLastNum] = useState(1);
+    const [lastNum, setLastNum] = useState(0);
     const [lastDie, setLastDie] = useState(0);
-    const [rollBtn, setRollBtn] = useState(true);
     const [userDieChoice, setUserDieChoice] = useState(true);
     const [totalDice, setTotalDice] = useState(players*dice);
-    const [allPlayersDice, setAllPlayersDice] = useState([[1,5,2,3,2],[2,5,6,1,3,4],[5,6,1,2,4,4,4]]);
-    const [CPU1] = CPU(dice);
-    const [CPU2] = CPU(dice);
-    const [CPU3] = CPU(dice);
-    const [CPU4] = CPU(dice);
-    const [CPU5] = CPU(dice);
+    const [allPlayersDice, setAllPlayersDice] = useState();
+    const [playerCalls, setPlayerCalls] = useState(false);
+    const [whosTurn, setWhosTurn] = useState('player');
+    const [CPU1, CPU1Name] = CPU(dice, rolling);
+    const [CPU2, CPU2Name] = CPU(dice, rolling);
+    const [CPU3, CPU3Name] = CPU(dice, rolling);
+    const [CPU4, CPU4Name] = CPU(dice, rolling);
+    const [CPU5, CPU5Name] = CPU(dice, rolling);
 
     const startGame = e => {
         e.preventDefault();
         setStartDice([]);
-        for(let i=0; i<dice; i++){
-            setStartDice(prevState => [...prevState, i]);
-        }
+        for(let i=0; i<dice; i++) setStartDice(prevState => [...prevState, i]);
         props.setGameStart(true);
-        console.log(CPU1)
         setAllPlayersDice(()=>{
             switch(players){
-                case 2: [numArr, CPU1, CPU2]; break;
-                case 3: [numArr, CPU1, CPU2, CPU3]; break;
-                case 4: [numArr, CPU1, CPU2, CPU3, CPU4]; break;
-                case 5: [numArr, CPU1, CPU2, CPU3, CPU4, CPU5]; break;
+                case 2: return {player: numArr, CPU1: CPU1, CPU2: CPU2};
+                case 3: return {player: numArr, CPU1: CPU1, CPU2: CPU2, CPU3: CPU3};
+                case 4: return {player: numArr, CPU1: CPU1, CPU2: CPU2, CPU3: CPU3, CPU4: CPU4};
+                case 5: return {player: numArr, CPU1: CPU1, CPU2: CPU2, CPU3: CPU3, CPU4: CPU4, CPU5: CPU5};
             }
-        })
-        console.log(allPlayersDice)
+        });
     }
 
 
@@ -49,39 +49,74 @@ const Game = props => {
             []
         })
     }
-
-    const chooseDice = () => {
-    }
     
     const rollDice = sound => {
-        setRollBtn(false);
+        setRolling(true);
         if(sound){
             const diceSound = new Audio('/dicehand.m4a');
             diceSound.play();
             return setTimeout(()=>setRolling(true),2325);
         }
-        setRolling(true);
     }
     
-    const callBet = (lastBet, lastDie, playerBetting, playerCalling) => {
+    const callBet = (playerBetting, playerCalling) => {
         const bet = 0;
-        totalDice.forEach(val=>{
+        const allDice = [];
+        setPlayerCalls(true);
+        for(let key in allPlayersDice){
+            allDice.push(...allPlayersDice[key]);
+        }
+
+        allDice.forEach(val=>{
             if(val === lastDie) bet++;
-        })
-        if(bet >= lastBet) return setPlayerDice(playerCalling, false);
+        });
+
+        if(bet >= lastNum) return setPlayerDice(playerCalling, false);
         return setPlayerDice(playerBetting, false);
     }
 
-    const cpuBet = () => {
-
+    const cpuBetOrCall = () => {
+        console.log(players)
+        if(whosTurn === `CPU${players}`) return setWhosTurn('player');
+        setWhosTurn(`CPU${whosTurn+1}`);
+        if(lastNum > totalDice*.75 && odds() > 10){
+            callBet(whosTurn, whosTurn-1);
+        }
+        
+        // allPlayersDice[whosTurn]
+        
     }
+    
+    useEffect(()=>{
+        if(whosTurn !== 'player') cpuBetOrCall();
+    },[whosTurn]);
 
-    const userMakeBet = e => {
-        e.preventDefault();
-        setLastDie(userDieChoice);
-        setLastNum(numChoice);
-        // Have CPU make their bets here
-        cpuBet();
+    const whoCalled = () => {
+        let playerCalling;
+        let playerCalled;
+        switch(whosTurn){
+            case 0: playerCalling = 'You'; break;
+            case 1: playerCalling = CPU1Name; break;
+            case 2: playerCalling = CPU2Name; break;
+            case 3: playerCalling = CPU3Name; break;
+            case 4: playerCalling = CPU4Name; break;
+            case 5: playerCalling = CPU5Name; break;
+            case 6: playerCalling = CPU6Name; break;
+        }
+
+        switch(whosTurn-1){
+            case 5: playerCalled = 'You'; break;
+            case 0: playerCalled = CPU1Name; break;
+            case 1: playerCalled = CPU2Name; break;
+            case 2: playerCalled = CPU3Name; break;
+            case 3: playerCalled = CPU4Name; break;
+            case 4: playerCalled = CPU5Name; break;
+            case -1: playerCalled = CPU6Name; break;
+        }
+
+        return (
+            <span>{playerCalling} called {playerCalled}!  Show your hand!</span>
+        )
     }
 
     useEffect(()=>{
@@ -106,71 +141,39 @@ const Game = props => {
                                 pip={dice < 9 ? 0 : 1}
                                 />
                         ))}
-                        {rollBtn &&
+                        {!rolling &&
                             <button type='button' className='roll-dice' onClick={() => rollDice(props.sound)}>Roll the dice!</button>
                         }
                     </div>
-                    {rolling &&
-                        <div className='game-choice'>
-                            <form onSubmit={userMakeBet}>
-                                <label htmlFor='number'>Make&nbsp;your&nbsp;selection:</label>
-                                <span
-                                className='game-bet'
-                                name='number'
-                                value={numChoice}
-                                onChange={(e)=>setNumChoice(e.target.value)}
-                                >
-                                    {numChoice}
-                                    <span className='game-bet-down' onClick={()=>numChoice > lastNum ? setNumChoice(numChoice-1) : null}>&lt;</span>
-                                    <span className='game-bet-up' onClick={()=>numChoice < totalDice ? setNumChoice(numChoice+1) : null}>&lt;</span>
-                                </span>
-                                <div className='game-bet-dice'>
-                                    <Dice selectDie={setUserDieChoice} selection={userDieChoice} number={1} dice={2} pip={2}/>
-                                    <Dice selectDie={setUserDieChoice} selection={userDieChoice} number={2} dice={2} pip={2}/>
-                                    <Dice selectDie={setUserDieChoice} selection={userDieChoice} number={3} dice={2} pip={2}/>
-                                    <Dice selectDie={setUserDieChoice} selection={userDieChoice} number={4} dice={2} pip={2}/>
-                                    <Dice selectDie={setUserDieChoice} selection={userDieChoice} number={5} dice={2} pip={2}/>
-                                    <Dice selectDie={setUserDieChoice} selection={userDieChoice} number={6} dice={2} pip={2}/>
-                                </div>
-                                <button className='game-bet-btn'>Choose</button>
-                                <button type='button' className='game-call-btn' onClick={()=>callBet(lastBet, lastDie)}>Call</button>
-                            </form>
+                    {rolling && whosTurn === 'player' &&
+                        <Choice
+                        setLastDie={setLastDie}
+                        setLastNum={setLastNum}
+                        numChoice={numChoice}
+                        cpuBetOrCall={cpuBetOrCall}
+                        setNumChoice={setNumChoice}
+                        lastNum={lastNum}
+                        totalDice={totalDice}
+                        userDieChoice={userDieChoice}
+                        setUserDieChoice={setUserDieChoice}
+                        callBet={callBet} />
+                    }
+                    {playerCalls && 
+                        <div className='game-bet-called'>
+                            <span>
+                                {whoCalled()}
+                            </span>
                         </div>
                     }
                 </>
                 :
-                <div className='parameters-container'>
-                    <form onSubmit={startGame}>
-                        <div className='parameters'>
-                            <fieldset>
-                                <label htmlFor='CPU'>How many CPU players?</label>
-                                <select value={players} onChange={e => setPlayers(parseInt(e.target.value))}>
-                                    <option value={2}>2</option>
-                                    <option value={3}>3</option>
-                                    <option value={4}>4</option>
-                                    <option value={5}>5</option>
-                                </select>
-                            </fieldset>
-
-                            <fieldset>
-                                <label>How many dice would you like to start with?</label>
-                                <select value={dice} onChange={e => setDice(parseInt(e.target.value))}>
-                                    <option value={5}>5</option>
-                                    <option value={6}>6</option>
-                                    <option value={7}>7</option>
-                                    <option value={8}>8</option>
-                                    <option value={9}>9</option>
-                                    <option value={10}>10</option>
-                                </select>
-                            </fieldset>
-
-                        </div>
-                        <button className='btn-start'>Start Game!</button>
-                    </form>
-                    <Link to='/' className='btn-menu'>
-                        <button>Main Menu</button>
-                    </Link>
-                </div>
+                <Parameters
+                setDice={setDice}
+                setPlayers={setPlayers}
+                startGame={startGame}
+                dice={dice}
+                players={players}
+                />
             }
         </>
     )
